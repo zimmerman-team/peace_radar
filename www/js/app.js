560eeef9850732e5236aa2b77fd9a774bedc4551
daan <daniel@zimmermanzimmerman.nl>
2015-06-02 14:54:03 +0200

    first commit

---
+++ www/js/app.js
@@ -0,0 +1,121 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var app = angular.module('starter', ['ionic'])
+app.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/')
+
+  $stateProvider.state('navigation', {
+    url: '/',
+    templateUrl: 'templates/navigation.html'
+  });
+  $stateProvider.state('share_your_data', {
+    url: '/share_your_data',
+    templateUrl: 'templates/share_your_data.html',
+    controller: 'ShareDataCtrl'
+      
+
+  });
+  
+  $stateProvider.state('peace_barometer', {
+    url: '/',
+    templateUrl: 'templates/peace_barometer.html',
+    controller:'PeaceBarometerCtrl'
+  });
+  $stateProvider.state('diary', {
+    url: '/',
+    templateUrl: 'templates/diary.html',
+    controller:'DiaryCtrl'
+  });
+  $stateProvider.state('peace_radar', {
+    url: '/',
+    templateUrl: 'templates/peace_radar.html',
+    controller:'PeaceRadarCtrl'
+
+  });
+  $stateProvider.state('neighbourhood', {
+    url: '/',
+    templateUrl: 'templates/neighbourhood.html',
+    controller:'NeighbourhoodCtrl'
+  });
+  $stateProvider.state('about', {
+    url: '/',
+    templateUrl: 'templates/about.html',
+    controller:'AboutCtrl'
+
+  });
+  $stateProvider.state('personal_settings', {
+    url: '/',
+    templateUrl: 'templates/personal_settings.html'
+  });
+  $stateProvider.state('kill_data', {
+    url: '/',
+    templateUrl: 'templates/kill_data.html'
+  });
+})
+
+
+app.controller('ShareDataCtrl', function($scope) {
+  $scope.todos = [
+      {title: "Take out the trash", done: true},
+      {title: "Do laundry", done: false},
+      {title: "Start cooking dinner", done: false}
+   ]
+})
+
+app.controller('PeaceBarometerCtrl', function($scope) {
+  $scope.todos = [
+      {title: "Take out the trash", done: true},
+      {title: "Do laundry", done: false},
+      {title: "Start cooking dinner", done: false}
+   ]
+})
+
+app.controller('DiaryCtrl', function($scope) {
+  $scope.todos = [
+      {title: "Take out the trash", done: true},
+      {title: "Do laundry", done: false},
+      {title: "Start cooking dinner", done: false}
+   ]
+})
+
+app.controller('PeaceRadarCtrl', function($scope) {
+  $scope.todos = [
+      {title: "Take out the trash", done: true},
+      {title: "Do laundry", done: false},
+      {title: "Start cooking dinner", done: false}
+   ]
+})
+
+app.controller('NeighbourhoodCtrl', function($scope) {
+  $scope.todos = [
+      {title: "Take out the trash", done: true},
+      {title: "Do laundry", done: false},
+      {title: "Start cooking dinner", done: false}
+   ]
+})
+
+app.controller('AboutCtrl', function($scope) {
+  $scope.todos = [
+      {title: "Take out the trash", done: true},
+      {title: "Do laundry", done: false},
+      {title: "Start cooking dinner", done: false}
+   ]
+})
+
+
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
