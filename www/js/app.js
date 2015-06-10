71cffe0f648a5fdbe316bc0b191b23690fee4f2e
Daan <daan@zimmermanzimmerman.nl>
2015-06-10 16:19:52 +0200

    flink wat updates

--- www/js/app.js
+++ www/js/app.js
@@ -105,6 +105,37 @@ app.controller('AboutCtrl', function($scope) {
    ]
 })
 
+app.controller('MyCtrl', function($scope, $timeout) {
+    $scope.data = { 'mood' : '6' };
+    
+    var timeoutId = null;
+    
+    $scope.$watch('data.mood', function() {
+        
+        
+        console.log('Has changed');
+        
+        if(timeoutId !== null) {
+            console.log('Ignoring this movement');
+            return;
+        }
+        
+        console.log('Not going to ignore this one');
+        timeoutId = $timeout( function() {
+            
+            console.log('It changed recently!');
+            
+            $timeout.cancel(timeoutId);
+            timeoutId = null;
+            
+            // Now load data from server 
+        }, 1000); 
+        
+        
+    });
+    
+})
+
 
 
 .run(function($ionicPlatform) {
