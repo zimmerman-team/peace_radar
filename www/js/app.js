8c94709c6ca6ee9041480c0c42f4cb15b3548e95
Daan <daan@zimmermanzimmerman.nl>
2015-06-11 15:14:24 +0200

    Alpha versie

--- www/js/app.js
+++ www/js/app.js
@@ -3,150 +3,95 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('starter', ['ionic'])
-app.config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/')
-
-  $stateProvider.state('navigation', {
-    url: '/',
-    templateUrl: 'templates/navigation.html'
-  });
-  $stateProvider.state('share_your_data', {
-    url: '/share_your_data',
-    templateUrl: 'templates/share_your_data.html',
-    controller: 'ShareDataCtrl'
-      
-
-  });
-  
-  $stateProvider.state('peace_barometer', {
-    url: '/',
-    templateUrl: 'templates/peace_barometer.html',
-    controller:'PeaceBarometerCtrl'
-  });
-  $stateProvider.state('diary', {
-    url: '/',
-    templateUrl: 'templates/diary.html',
-    controller:'DiaryCtrl'
-  });
-  $stateProvider.state('peace_radar', {
-    url: '/',
-    templateUrl: 'templates/peace_radar.html',
-    controller:'PeaceRadarCtrl'
-
-  });
-  $stateProvider.state('neighbourhood', {
-    url: '/',
-    templateUrl: 'templates/neighbourhood.html',
-    controller:'NeighbourhoodCtrl'
-  });
-  $stateProvider.state('about', {
-    url: '/',
-    templateUrl: 'templates/about.html',
-    controller:'AboutCtrl'
-
-  });
-  $stateProvider.state('personal_settings', {
-    url: '/',
-    templateUrl: 'templates/personal_settings.html'
-  });
-  $stateProvider.state('kill_data', {
-    url: '/',
-    templateUrl: 'templates/kill_data.html'
-  });
-})
-
-
-app.controller('ShareDataCtrl', function($scope) {
-  $scope.todos = [
-      {title: "Take out the trash", done: true},
-      {title: "Do laundry", done: false},
-      {title: "Start cooking dinner", done: false}
-   ]
-})
-
-app.controller('PeaceBarometerCtrl', function($scope) {
-  $scope.todos = [
-      {title: "Take out the trash", done: true},
-      {title: "Do laundry", done: false},
-      {title: "Start cooking dinner", done: false}
-   ]
-})
-
-app.controller('DiaryCtrl', function($scope) {
-  $scope.todos = [
-      {title: "Take out the trash", done: true},
-      {title: "Do laundry", done: false},
-      {title: "Start cooking dinner", done: false}
-   ]
-})
-
-app.controller('PeaceRadarCtrl', function($scope) {
-  $scope.todos = [
-      {title: "Take out the trash", done: true},
-      {title: "Do laundry", done: false},
-      {title: "Start cooking dinner", done: false}
-   ]
-})
-
-app.controller('NeighbourhoodCtrl', function($scope) {
-  $scope.todos = [
-      {title: "Take out the trash", done: true},
-      {title: "Do laundry", done: false},
-      {title: "Start cooking dinner", done: false}
-   ]
-})
-
-app.controller('AboutCtrl', function($scope) {
-  $scope.todos = [
-      {title: "Take out the trash", done: true},
-      {title: "Do laundry", done: false},
-      {title: "Start cooking dinner", done: false}
-   ]
-})
-
-app.controller('MyCtrl', function($scope, $timeout) {
-    $scope.data = { 'mood' : '6' };
-    
-    var timeoutId = null;
-    
-    $scope.$watch('data.mood', function() {
-        
-        
-        console.log('Has changed');
-        
-        if(timeoutId !== null) {
-            console.log('Ignoring this movement');
-            return;
-        }
-        
-        console.log('Not going to ignore this one');
-        timeoutId = $timeout( function() {
-            
-            console.log('It changed recently!');
-            
-            $timeout.cancel(timeoutId);
-            timeoutId = null;
-            
-            // Now load data from server 
-        }, 1000); 
-        
-        
-    });
-    
-})
-
-
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('app', {
+    url: "/app",
+    abstract: true,
+    templateUrl: "templates/menu.html",
+    controller: 'AppCtrl'
+  })
+
+  .state('opener', {
+    url: "/opener",
+    templateUrl: "templates/opener.html"
+  })
+
+  .state('app.share_your_data', {
+    url: "/share_your_data",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/share_your_data.html"
+      }
+    }
+  })
+
+  .state('app.about', {
+    url: "/about",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/about.html"
+      }
+    }
+  })
+
+  .state('app.personal_settings', {
+    url: "/personal_settings",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/personal_settings.html"
+      }
+    }
+  })
+
+  .state('app.kill_data', {
+    url: "/kill_data",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/kill_data.html"
+      }
+    }
+  })
+
+  .state('app.peace_barometer', {
+    url: "/peace_barometer",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/peace_barometer.html"
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('opener');
+});
+
+// Add the 'ionic.service.core' module to your main angular module:
+angular.module('test', ['ionic.service.core'])
+// Identify App
+.config(['$ionicAppProvider', function($ionicAppProvider) {
+  // Identify app
+  $ionicAppProvider.identify({
+    // The App ID for the server
+    app_id: '8e26ce6c',
+    // The API key all services will use for this app
+    api_key: '224c97f41cd5cc9d8bcdce0b55d75aa4a5e5ce671fabccf9'
+  });
+}]);
---
+++ www/js/controllers.js
@@ -0,0 +1,44 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $ionicPlatform, $ionicNavBarDelegate) {
+
+  $scope.goBack = function() {
+    $ionicNavBarDelegate.back();
+  };
+
+  $scope.data = { 'mood' : '6' };
+  
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+  // $ionicModal.fromTemplateUrl('templates/opener.html', {
+  //   scope: $scope,
+  //   animation: 'slide-in-up'
+  // }).then(function(modal) {
+  //   $scope.modal = modal;
+  // });
+  // $scope.openModal = function() {
+  //   $scope.modal.show();
+  // };
+  // $scope.closeModal = function() {
+  //   $scope.modal.hide();
+  // };
+  // $ionicPlatform.ready(function(){
+  //   $scope.openModal();
+  // });
+  // //Cleanup the modal when we're done with it!
+  // $scope.$on('$destroy', function() {
+  //   $scope.modal.remove();
+  // });
+  // // Execute action on hide modal
+  // $scope.$on('modal.hidden', function() {
+  //   // Execute action
+  // });
+  // // Execute action on remove modal
+  // $scope.$on('modal.removed', function() {
+  //   // Execute action
+  // });
+});
