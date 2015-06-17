8e1b0574d7aac60159400f5054bf476002b9707b
Daan <daan@zimmermanzimmerman.nl>
2015-06-17 14:33:14 +0200

    prep voor location services

--- www/js/app.js
+++ www/js/app.js
@@ -4,8 +4,8 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
+//angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova', 'ngCordovaOauth'])
 angular.module('starter', ['ionic', 'starter.controllers'])
-
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -23,16 +23,22 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
-  .state('app', {
-    url: "/app",
-    abstract: true,
-    templateUrl: "templates/menu.html",
-    controller: 'AppCtrl'
+  .state('signin', {
+    url: '/sign-in',
+    templateUrl: 'templates/sign-in.html',
+    controller: 'LoginCtrl'
   })
 
   .state('opener', {
     url: "/opener",
     templateUrl: "templates/opener.html"
+  })
+
+  .state('app', {
+    url: "/app",
+    abstract: true,
+    templateUrl: "templates/menu.html",
+    controller: 'AppCtrl'
   })
 
   .state('app.share_your_data', {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,54 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ngCordova'])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout, $ionicPlatform, $ionicHistory, $ionicPopup) {
+// .controller('LoginCtrl', function($scope, $cordovaOauth, $state, LoginService)) {
+
+//   $scope.data = {};
+ 
+//   $scope.login = function() {
+//     LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
+//       $state.go('app.personal_settings');
+//     }).error(function(data) {
+//       var alertPopup = $ionicPopup.alert({
+//         title: 'Login failed!',
+//         template: 'Please check your credentials!'
+//       });
+//     });
+//   }
+
+//   $scope.twitterlogin = function() {
+//     $cordovaOauth.twitter("CLIENT_ID_HERE", ["email"]).then(function(result) {
+//       // results
+//     }, function(error) {
+//       // error
+//     });
+//   }
+// })
+
+// .service('LoginService', function($q) {
+//     return {
+//         loginUser: function(name, pw) {
+//             var deferred = $q.defer();
+//             var promise = deferred.promise;
+ 
+//             if (name == 'user' && pw == 'secret') {
+//                 deferred.resolve('Welcome ' + name + '!');
+//             } else {
+//                 deferred.reject('Wrong credentials.');
+//             }
+//             promise.success = function(fn) {
+//                 promise.then(fn);
+//                 return promise;
+//             }
+//             promise.error = function(fn) {
+//                 promise.then(null, fn);
+//                 return promise;
+//             }
+//             return promise;
+//         }
+//     }
+// })
+
+.controller('AppCtrl', function($scope, $timeout, $ionicPlatform, $ionicHistory, $ionicPopup, $cordovaOauth, $cordovaBackgroundGeolocation) {
 
   $scope.goBack = function() {
     $ionicHistory.goBack();
@@ -12,13 +60,43 @@ angular.module('starter.controllers', [])
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
   // listen for the $ionicView.enter event:
-  $scope.$on('$ionicView.enter', function(e) {
-  });
+  $scope.$on('$ionicView.enter', function(e) {});
   $scope.showThanks = function() {
     var alertPopup = $ionicPopup.alert({
       title: 'Mood submitted',
       template: 'Thanks very much!'
     });
+   };
 
-    };
+  // Add BackgroundGeolocation event-listeners when Platform is ready.
+  ionic.Platform.ready(function() {
+    var bgGeo = BackgroundGeolocation.getPlugin();
+  });
+
+  /**
+  * BackgroundGelocation plugin state
+  */
+  $scope.bgGeo = {
+    enabled: window.localStorage.getItem('bgGeo:enabled') == 'true',
+    started: window.localStorage.getItem('bgGeo:started') == 'true'
+  };
+
+  /**
+  * Enable BackgroundGeolocation
+  */
+  $scope.onToggleEnabled = function() {
+    var isEnabled = $scope.bgGeo.enabled;
+    console.log('onToggleEnabled: ', isEnabled);
+    BackgroundGeolocation.setEnabled(isEnabled);
+    
+    var alertPopup = $ionicPopup.alert({
+      title: 'Location',
+      template: 'hier locatie'
+    });
+
+    if (!isEnabled) {
+      $scope.bgGeo.started = false;
+    }
+  };
+
 });
